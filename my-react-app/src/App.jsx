a239f5bd65caaaf8cbd7e753fa1fd354da0b2370
opheliechoisi <ochoisi@yahoo.com>
2025-06-11 18:52:04 +0200

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="text">
+      <h1>Lorem</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sit
+        aliquam excepturi accusantium sed sunt doloribus, est itaque cumque,
+        unde dicta nam voluptates aut! Magnam laudantium voluptates maiores iure
+        beatae.
+      </p>
+    </div>
+  );
+}
+
+export default App;
